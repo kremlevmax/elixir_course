dcccfe98a64096b513b2184b68ea747a96ac7b8d
kremlevmax <kremlevmax1989@gmail.com>
2022-10-28 23:17:03 -0700

    Reduce function

---
+++ reduce_func.exs
@@ -0,0 +1,11 @@
+votes = [
+  %{district: 1, candidate: 1, votes: 230},
+  %{district: 1, candidate: 2, votes: 122},
+  %{district: 1, candidate: 3, votes: 14},
+  %{district: 2, candidate: 1, votes: 324},
+  %{district: 2, candidate: 2, votes: 222},
+  %{district: 2, candidate: 3, votes: 80}
+]
+
+second_district = Enum.filter(votes, fn x -> x.district == 2 end)
+IO.inspect(second_district)
